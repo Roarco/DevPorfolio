bbc2c9a71b902b59989cfde7fcd2c61d0d251ac8
Roarco <sebuscaroberth@gmail.com>
2022-06-29 12:23:38 -0500

    solucionando problema deploy4

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(=+6t3^9=g7aw259*ik0=f84dde_abc9*-!sf8cjdo3%j+$&^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arrcon.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,9 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
